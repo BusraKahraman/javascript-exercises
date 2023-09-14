836b0f9cb3050d2d1f6dca10924a6afd69dcd461
BusraKahraman <busrakahraman.b@gmail.com>
2023-09-14 14:58:53 +0300

    fundamentals part 4 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let n = Math.abs(num2 - num1) + 1;
+  let sum = (n * (num1 + num2)) / 2;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return 'ERROR';
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let convert = ((f - 32) * 5) / 9;
+  return Math.round(convert * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let convert = (c * 9) / 5 + 32;
+  return Math.round(convert * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
